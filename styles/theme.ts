393f2922f41e8d79a9efd3a9d9a144c87471643a
Jonah <jonahgoh@outlook.com>
2021-07-11 19:45:35 +0800

    Add background color

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,7 +1,35 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
 const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false
 };
-const theme = extendTheme({ config });
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode('gray.700', 'whiteAlpha.900')(props),
+      bg: mode('gray.50', 'gray.900')(props),
+      fontSize: '1.2em',
+      '.deleted': {
+        color: '#ff8383 !important',
+        fontStyle: 'normal !important'
+      },
+      '.inserted': {
+        color: '#b5f4a5 !important',
+        fontStyle: 'normal !important'
+      }
+    },
+    a: {
+      color: mode('blue.500', 'blue.200')(props),
+      transition: 'color 0.15s',
+      transitionTimingFunction: 'ease-out',
+      fontWeight: '500',
+      _hover: {
+        color: mode('blue.600', 'blue.300')(props)
+      }
+    }
+  })
+};
+const theme = extendTheme({ config, styles });
 export default theme;
