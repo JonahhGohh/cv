862ec11f2c096ef9f6eca2b4ed87949f9c9f5785
Jonah <jonahgoh@outlook.com>
2021-07-12 21:53:30 +0800

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from '@chakra-ui/layout';
+
+export default function Custom404() {
+  return (
+    <>
+      <Box>
+        <Text>Error 404 - Page Not Found</Text>
+      </Box>
+    </>
+  );
+}
